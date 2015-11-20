7b07fd931b12fb53c2fc02bb326414bd75f89df1
Oleksandr Polieno (nanvel) <polyenoom@gmail.com>
2015-11-20 23:59:52 +0200

    fixed git pull

--- c2p2/utils/github.py
+++ c2p2/utils/github.py
@@ -11,14 +11,11 @@ __all__ = ('github_pull',)
 def github_pull():
 
     sub_process = Subprocess(
-        'git',
+        ['git', 'pull', 'origin', settings.GITHUB_BRANCH],
         stdin=Subprocess.STREAM,
         stdout=Subprocess.STREAM,
         stderr=Subprocess.STREAM
     )
-
-    yield gen.Task(sub_process.stdin.write, ('pull origin {branch}'.format(branch=settings.GITHUB_BRANCH)).encode())
-    sub_process.stdin.close()
 
     result, error = yield [
         gen.Task(sub_process.stdout.read_until_close),
